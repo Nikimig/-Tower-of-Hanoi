9032a255129723c4570af97a3315694ebc5289c6
Nikimig <143002121+Nikimig@users.noreply.github.com>
2024-03-04 13:40:08 +0300

    Add files via upload

--- Ханойская башня/hann.js
+++ Ханойская башня/hann.js
@@ -1,33 +1,41 @@
-var divRing = '<div class = "ring $color"></div>';
+var divRing = '<div id="r$number" class = "ring $color"></div>';
 var divPole = '<div class="pole $numder"></div>';
-const color = ['red', 'orange', 'yellow', 'green','lite_blue', 'blue', 'purple' ];
-
+var divSector = '<div id="s$number" class="sectors $number"></div>'
+const color = ['red', 'orange', 'yellow', 'green','liteblue', 'blue', 'purple' ];
+width_ring = 58
 $(function (){
    console.log('hann');
    addPole();
+   addSectors();
    addRings();
    setDraggable();
+   setDroppable();
+
 
 
 });
 
 function addPole(){
-   $('.one').html('');
-   $('.one').append(divPole.replace('$number', '1'));
+   document.getElementById('p1').innerHTML += divPole.replace('$number', '1');
+   document.getElementById('p2').innerHTML += divPole.replace('$number', '2');
+   document.getElementById('p3').innerHTML += divPole.replace('$number', '3');}
 
-   $('.two').html('');
-   $('.two').append(divPole.replace('$number', '2'));
-
-   $('.three').html('');
-   $('.three').append(divPole.replace('$number', '3'));
-
-}
+function addSectors(){
+   for (let i = 0; i < 7; ++i ){
+         document.getElementById('p1').innerHTML += divSector.replace('$number', i).replace('$number', i)
+         //document.getElementById('s'+ String(i)).style.setProperty('margin-top', String(172+(70*(i+1))) + 'px');
+         document.getElementById('p2').innerHTML += divSector.replace('$number', i).replace('$number', i)
+         document.getElementById('p3').innerHTML += divSector.replace('$number', i).replace('$number', i)
+}}
 
 function addRings() {
-   $('.one').append(divRing.replace('$color', color[0]));
-   $('.one').append(divRing.replace('$color', color[1]));
-   console.log(color.push(1))
-}
+   for (let i = 0; i < 7; ++i ){
+   //$('.one').append(divRing.replace('$color', color[0])); это код для jqerry
+      document.getElementById('s' + i).innerHTML += divRing.replace('$color', color[i]).replace('$number', i );
+      console.log(document.querySelector('.' + color[i]));
+      document.querySelector('.' + color[i]).style.setProperty('background-color',  color[i]);
+      document.querySelector('.' + color[i]).style.setProperty('width', 98 + (width_ring * i) + 'px');
+}}
 
 function setDraggable() {
    $('.ring').draggable();
@@ -36,9 +44,10 @@ function setDraggable() {
 function setDroppable() {
    $('.pole-label').droppable({
       drop: function(event, ui){
-         var frCoord = ui.draggable.attr('id').substring(1);
-         var toCoord = this.id.substring(1);
+         var frCoord = document.getElementById('p1').id
+         var toCoord = this.id;
          console.log(frCoord,toCoord);
+         console.log()
       }
    })
 }
