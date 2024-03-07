8e263df8b41435c0def81d494f5bf7d70eb6b663
Nikimig <143002121+Nikimig@users.noreply.github.com>
2024-03-07 14:43:29 +0300

    Add files via upload

--- Ханойская башня/hann.js
+++ Ханойская башня/hann.js
@@ -1,6 +1,7 @@
 var divRing = '<div id="r$number" class = "ring $color"></div>';
 var divPole = '<div class="pole $number"></div>';
 var divSector = '<div id="s$number" class="sector $number"></div>';
+var divBigSektor = '<div id="sek $number" class="sectorsAdd $number"></div>'
 const color = ['red', 'orange', 'yellow', 'green','lightblue', 'blue', 'purple' ];
 
 $(function (){
@@ -13,14 +14,15 @@ $(function (){
 });
 
 function addPole(){
-   document.getElementById('p1').innerHTML += divPole.replace('$number', '1')
-   document.getElementById('p2').innerHTML += divPole.replace('$number', '2')
-   document.getElementById('p3').innerHTML += divPole.replace('$number', '3')
+   for (let i = 1; i < 4; ++i ){
+      document.getElementById('p'+ String(i)).innerHTML += divPole.replace('$number', i)
+      //document.getElementById('p2').innerHTML += divPole.replace('$number', '2')
+      //document.getElementById('p3').innerHTML += divPole.replace('$number', '3')
 
-   document.getElementById('p1').innerHTML += '<div id="sek $number" class="sectorsAdd $number"></div>'.replace('$number', '1').replace('$number', '1');
-   document.getElementById('p2').innerHTML += '<div id="sek $number" class="sectorsAdd $number"></div>'.replace('$number', '2').replace('$number', '2');
-   document.getElementById('p3').innerHTML += '<div id="sek $number" class="sectorsAdd $number"></div>'.replace('$number', '3').replace('$number', '3');
-}
+      document.getElementById('p'+ String(i)).innerHTML += divBigSektor.replace('$number', i).replace('$number', i);
+      //document.getElementById('p2').innerHTML += '<div id="sek $number" class="sectorsAdd $number"></div>'.replace('$number', '2').replace('$number', '2');
+      //document.getElementById('p3').innerHTML += '<div id="sek $number" class="sectorsAdd $number"></div>'.replace('$number', '3').replace('$number', '3');
+}}
 
 function addRingsAndSectors() {
    for (let i = 0; i < 7; ++i ){
@@ -30,7 +32,7 @@ function addRingsAndSectors() {
       document.getElementById('sek 3').innerHTML += divSector.replace('$number','3' + i).replace('$number','3' + i)
       
       //Добавляем кольца
-      document.getElementById('s'+ '2' + i).innerHTML += divRing.replace('$color', color[i]).replace('$number', i );
+      document.getElementById('s'+ '1' + i).innerHTML += divRing.replace('$color', color[i]).replace('$number', i );
       //console.log(document.querySelector('.' + color[i]));
       
       //Описываем стили колец
@@ -50,43 +52,39 @@ function setDroppable() {
          var frCoord2 = ui.draggable.parent().attr('id');
          var toCoord = this.id.substring(4);
          console.log(frCoord,toCoord);
-         //console.log(('.' + String(document.getElementById('s'+ '2' + 2).childNodes[0].className).substring(5)))
-         //console.log(Number(document.querySelector('#' + ui.draggable.attr('id')).parentElement.id.substring(2)))
-         //console.log($('.' + String(document.getElementById('s'+ '2' + 2).childNodes[0].className).substring(5)))
-         //$('.' + String(document.getElementById('s'+ '2' + 2).childNodes[0].className).substring(5)).draggable({ containment: ".hann-label", scroll: false });
-         //document.getElementById(toCoord).innerHTML += document.querySelector('#' + ui.draggable.attr('id'))
-         let div = document.querySelector('#' + ui.draggable.attr('id'))
-         let id_el = Number(document.querySelector('#' + ui.draggable.attr('id')).parentElement.id.substring(2));
+         var div = document.querySelector('#' + ui.draggable.attr('id'))
+         var id_el = Number(document.querySelector('#' + ui.draggable.attr('id')).parentElement.id.substring(2));
          console.log(id_el)
-         console.log
-
-
-         if ( id_el < 6 && (document.getElementById('s'+ frCoord + (id_el + 1))).hasChildNodes()){
-            $('.' + String(document.getElementById('s'+ frCoord + (id_el + 1)).childNodes[0].className).substring(5)).draggable({ containment: ".hann-label", scroll: false });
-            $('.' + String(document.getElementById('s'+ frCoord + (id_el + 1)).childNodes[0].className.substring(5)).replace('ui-draggable ui-draggable-handle ui-draggable-dragging ui-draggable-disabled', '')).draggable("enable")
-            //console.log('.' + String(document.getElementById('s'+ frCoord + (id_el + 1)).childNodes[0].className.substring(5)).replace('ui-draggable ui-draggable-handle ui-draggable-dragging ui-draggable-disabled', ''))
-         }
 
 
          for (let i = 6; i > -1; --i ){
             if (document.getElementById('s'+ toCoord + i).hasChildNodes()==false || document.querySelector('#' + ui.draggable.attr('id')).parentElement == document.getElementById('s'+ toCoord + i)) {
-                  //console.log(i)
+               if ( i == 6 || Number(div.style.width.replace('px', '')) < Number(document.getElementById('s'+ String(toCoord) + (i+1)).childNodes[0].style.width.replace('px', ''))) {  //console.log(i)
                   div.remove()
                   document.getElementById('s'+ toCoord + i).innerHTML += div.outerHTML
                   document.querySelector('#' + ui.draggable.attr('id')).style.setProperty('top', 0 + 'px');
                   document.querySelector('#' + ui.draggable.attr('id')).style.setProperty('left', 0 + 'px');
-                  //console.log(String(document.getElementById('s'+ toCoord + i).childNodes[0].className).substring(5));
                   $('.' + String(document.getElementById('s'+ toCoord + i).childNodes[0].className).substring(5).replace('ui-draggable ui-draggable-handle ui-draggable-dragging', '')).draggable({ containment: ".hann-label", scroll: false });
 
                   if ( document.getElementById('s'+ toCoord + i).id.substring(2) < 6 && (document.getElementById('s'+ toCoord + (i + 1))).hasChildNodes()){
                      $('.' + String(document.getElementById('s'+ toCoord + (i + 1)).childNodes[0].className).substring(5).replace('ui-draggable ui-draggable-handle ui-draggable-dragging', '')).draggable("disable")}
-      
-                  
-               break} else {
-                  document.querySelector('#' + ui.draggable.attr('id')).style.setProperty('top', 0 + 'px');
-                  document.querySelector('#' + ui.draggable.attr('id')).style.setProperty('left', 0 + 'px');
-            }}
 
-}
-}                     
-)}
+                  if ( id_el < 6 && (document.getElementById('s'+ frCoord + (id_el + 1))).hasChildNodes()){
+                     $('.' + String(document.getElementById('s'+ frCoord + (id_el + 1)).childNodes[0].className).substring(5)).draggable({ containment: ".hann-label", scroll: false });
+                     $('.' + String(document.getElementById('s'+ frCoord + (id_el + 1)).childNodes[0].className.substring(5)).replace('ui-draggable ui-draggable-handle ui-draggable-dragging ui-draggable-disabled', '')).draggable("enable")
+                  }
+
+                  if ( id_el < 6 && (document.getElementById('s'+ frCoord + (id_el + 1))).hasChildNodes()){
+                     $('.' + String(document.getElementById('s'+ frCoord + (id_el + 1)).childNodes[0].className).substring(5)).draggable({ containment: ".hann-label", scroll: false });
+                     $('.' + String(document.getElementById('s'+ frCoord + (id_el + 1)).childNodes[0].className.substring(5)).replace('ui-draggable ui-draggable-handle ui-draggable-dragging ui-draggable-disabled', '')).draggable("enable")}
+                  
+                  break}else{
+                     div.style.setProperty('top', 0 + 'px');
+                     div.style.setProperty('left', 0 + 'px');
+                     $('.' + String(document.querySelector('#' + ui.draggable.attr('id')).className.substring(5)).replace('ui-draggable ui-draggable-handle ui-draggable-dragging', '')).draggable("enable");
+                     console.log(String(document.querySelector('#' + ui.draggable.attr('id')).className.substring(5)).replace('ui-draggable ui-draggable-handle ui-draggable-dragging', ''));
+                     break}} 
+            else {
+               div.style.setProperty('top', 0 + 'px');
+               div.style.setProperty('left', 0 + 'px');
+               $('.' + String(document.querySelector('#' + ui.draggable.attr('id')).className.substring(5)).replace('ui-draggable ui-draggable-handle ui-draggable-dragging', '')).draggable("enable")}}}})}
